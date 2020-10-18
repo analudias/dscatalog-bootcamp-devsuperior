9b15e4d09d8971247760d305003743cba0f22294
Ana Luisa Dias <analudias@live.com>
2020-10-18 19:02:31 -0300

    Add pagination configuration

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ProductsResponse } from 'core/types/Product';
+import Pagination from 'core/components/Pagination';
 import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
@@ -10,13 +11,14 @@ const Catalog = () => {
     //quando a lista de produtos estiver disponível, popular um estado no componente e listar os produtos dinâmicamente
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false); //por padrão, o estado deve ser falso pois o loading não deve aparecer. O loading só aparece quando fizermos a requisição
+    const [activePage, setActivePage] = useState(0);
 
     console.log(productsResponse);
 
     //quando o componente iniciar, buscar a lista de ProductRepository
     useEffect(() => {
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
         
@@ -28,7 +30,7 @@ const Catalog = () => {
                 //finalizar o loader
                 setIsLoading(false);
             });
-    }, []);
+    }, [activePage]); //toda as vezes em que for selecionada uma nova página, este código será executado novamente
 
     return (
         <div className="catalog-container">
@@ -44,6 +46,13 @@ const Catalog = () => {
                     ))
                 )}
             </div>
+            {productsResponse && (
+                <Pagination 
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />
+            )}
         </div>
     );
 };
