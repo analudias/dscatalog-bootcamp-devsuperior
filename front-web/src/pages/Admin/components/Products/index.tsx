cde25cf73fb8cb031a4f8640eaaf84c6e20f1956
Ana Luisa Dias <analudias@live.com>
2020-10-19 23:06:57 -0300

    Base structure for Admin form

--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Form from './Form';
+import List from './List';
 import './styles.scss';
 
 const Products = () => {
-
     return (
         <div>
-            <Link to="/admin/products" className="mr-5">
-                Listar produtos
-            </Link>
-            <Link to="/admin/products/create" className="mr-5">
-                Criar produtos
-            </Link>
-            <Link to="/admin/products/10" className="mr-5">
-                Editar produtos
-            </Link>
             <Switch>
                 <Route path="/admin/products" exact>
-                    <h1>Exibir a listagem de produtos</h1>
+                    <List />
                 </Route>
                 <Route path="/admin/products/create">
-                    <h1>Criar novo produto</h1>
+                    <Form />
                 </Route>
                 <Route path="/admin/products/:productId">
                     <h1>Editar um produto</h1>
